9a2f940f20078efe4b3a7483cbc20e6bbe1fd1d8
voidseg <voidseg@gmail.com>
2014-10-06 14:52:57 +0200

    ported jvctl to gtk3 and removed deprecated function calls

--- jvctl_legacy.py
+++ jvctl_legacy.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python
 
-#import pygtk
-#pygtk.require('2.0')
-#import gtk
-from gi.repository import Gtk
-from gi.repository import Gdk
-#from gi.repository import GObject
+import pygtk
+pygtk.require('2.0')
+import gtk
 import getopt
 import sys
 import liblo
 import math
 import threading
 import time
-import signal
 
-Gdk.threads_init()
-#gtk.gdk.threads_init()
+gtk.gdk.threads_init()
 
 def db_to_coeff(db):
 	if db > -318.8:
@@ -48,38 +43,38 @@ def db_to_fader(db):
 		return 0.0
 
 def scroll2str(scroll):
-	if scroll == Gtk.ScrollType.NONE:
-		return "NONE"
-	elif scroll == Gtk.ScrollType.JUMP:
-		return "JUMP"
-	elif scroll == Gtk.ScrollType.STEP_BACKWARD:
-		return "STEP_BACKWARD"
-	elif scroll == Gtk.ScrollType.STEP_FORWARD:
-		return "STEP_FORWARD"
-	elif scroll == Gtk.ScrollType.PAGE_BACKWARD:
-		return "PAGE_BACKWARD"
-	elif scroll == Gtk.ScrollType.PAGE_FORWARD:
-		return "PAGE_FORWARD"
-	elif scroll == Gtk.ScrollType.STEP_UP:
-		return "STEP_UP"
-	elif scroll == Gtk.ScrollType.STEP_DOWN:
-		return "STEP_DOWN"
-	elif scroll == Gtk.ScrollType.PAGE_UP:
-		return "PAGE_UP"
-	elif scroll == Gtk.ScrollType.PAGE_DOWN:
-		return "PAGE_DOWN"
-	elif scroll == Gtk.ScrollType.STEP_LEFT:
-		return "STEP_LEFT"
-	elif scroll == Gtk.ScrollType.STEP_RIGHT:
-		return "STEP_RIGHT"
-	elif scroll == Gtk.ScrollType.PAGE_LEFT:
-		return "PAGE_LEFT"
-	elif scroll == Gtk.ScrollType.PAGE_RIGHT:
-		return "PAGE_RIGHT"
-	elif scroll == Gtk.ScrollType.START:
-		return "START"
-	elif scroll == Gtk.ScrollType.END:
-		return "END"
+	if scroll == gtk.SCROLL_NONE:
+		return "SCROLL_NONE"
+	elif scroll == gtk.SCROLL_JUMP:
+		return "SCROLL_JUMP"
+	elif scroll == gtk.SCROLL_STEP_BACKWARD:
+		return "SCROLL_STEP_BACKWARD"
+	elif scroll == gtk.SCROLL_STEP_FORWARD:
+		return "SCROLL_STEP_FORWARD"
+	elif scroll == gtk.SCROLL_PAGE_BACKWARD:
+		return "SCROLL_PAGE_BACKWARD"
+	elif scroll == gtk.SCROLL_PAGE_FORWARD:
+		return "SCROLL_PAGE_FORWARD"
+	elif scroll == gtk.SCROLL_STEP_UP:
+		return "SCROLL_STEP_UP"
+	elif scroll == gtk.SCROLL_STEP_DOWN:
+		return "SCROLL_STEP_DOWN"
+	elif scroll == gtk.SCROLL_PAGE_UP:
+		return "SCROLL_PAGE_UP"
+	elif scroll == gtk.SCROLL_PAGE_DOWN:
+		return "SCROLL_PAGE_DOWN"
+	elif scroll == gtk.SCROLL_STEP_LEFT:
+		return "SCROLL_STEP_LEFT"
+	elif scroll == gtk.SCROLL_STEP_RIGHT:
+		return "SCROLL_STEP_RIGHT"
+	elif scroll == gtk.SCROLL_PAGE_LEFT:
+		return "SCROLL_PAGE_LEFT"
+	elif scroll == gtk.SCROLL_PAGE_RIGHT:
+		return "SCROLL_PAGE_RIGHT"
+	elif scroll == gtk.SCROLL_START:
+		return "SCROLL_START"
+	elif scroll == gtk.SCROLL_END:
+		return "SCROLL_END"
 
 
 class VolumeOSC(threading.Thread):
@@ -156,7 +151,7 @@ class VolumeGUI:
 
 	def destroy(self, widget, data=None):
 		print "destroy signal occurred"
-		Gtk.main_quit()
+		gtk.main_quit()
 
 	def send_master_osc(self, value):
 		self.osc.send_master(value)
@@ -203,15 +198,15 @@ class VolumeGUI:
 		self.fader_event(ch, gain_abs, send_osc)
 		self.lock.release()
 
-	def scale_event(self, scale, scrolltype, value):
-		#print "scroll=" + scroll2str(scrolltype)
-		if scrolltype == Gtk.ScrollType.STEP_FORWARD:
+	def scale_event(self, scale, scroll, value):
+		#print "scroll=" + scroll2str(scroll)
+		if scroll == gtk.SCROLL_STEP_FORWARD:
 			value += 0.01
-		elif scrolltype == Gtk.ScrollType.STEP_BACKWARD:
+		elif scroll == gtk.SCROLL_STEP_BACKWARD:
 			value -= 0.01
-		elif scrolltype == Gtk.ScrollType.PAGE_FORWARD:
+		elif scroll == gtk.SCROLL_PAGE_FORWARD:
 			value += 0.1
-		elif scrolltype == Gtk.ScrollType.PAGE_BACKWARD:
+		elif scroll == gtk.SCROLL_PAGE_BACKWARD:
 			value -= 0.1
 		value = min(value, 1.0)
 		value = max(value, 0.0)
@@ -244,29 +239,17 @@ class VolumeGUI:
 
 		self.mute_event_safe(channel, not self.muted[channel], True)
 
-	def change_color(self, channel, mute):
-		if mute:
-			self.mutes[channel].override_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(65535,0,0,1))
-			self.mutes[channel].override_color(Gtk.StateFlags.ACTIVE, Gdk.RGBA(55535,0,0,1))
-			self.mutes[channel].override_color(Gtk.StateFlags.PRELIGHT, Gdk.RGBA(65535,0,0,1))
-		else:
-			self.mutes[channel].override_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0,65535,0,1))
-			self.mutes[channel].override_color(Gtk.StateFlags.ACTIVE, Gdk.RGBA(0,55535,0,1))
-			self.mutes[channel].override_color(Gtk.StateFlags.PRELIGHT, Gdk.RGBA(0,65535,0,1))
-
-#			if mute:
-#				self.mutes[channel].modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(65535,0,0))
-#				self.mutes[channel].modify_bg(gtk.STATE_ACTIVE, gtk.gdk.Color(55535,0,0))
-#				self.mutes[channel].modify_bg(gtk.STATE_PRELIGHT, gtk.gdk.Color(65535,0,0))
-#			else:
-#				self.mutes[channel].modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(0,65535,0))
-#				self.mutes[channel].modify_bg(gtk.STATE_ACTIVE, gtk.gdk.Color(0,55535,0))
-#				self.mutes[channel].modify_bg(gtk.STATE_PRELIGHT, gtk.gdk.Color(0,65535,0))
-
 	def set_mute(self, channel, mute):
 		if (self.muted[channel] != mute):
 			self.muted[channel] = mute
-			self.change_color(channel, mute)
+			if mute:
+				self.mutes[channel].modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(65535,0,0))
+				self.mutes[channel].modify_bg(gtk.STATE_ACTIVE, gtk.gdk.Color(55535,0,0))
+				self.mutes[channel].modify_bg(gtk.STATE_PRELIGHT, gtk.gdk.Color(65535,0,0))
+			else:
+				self.mutes[channel].modify_bg(gtk.STATE_NORMAL, gtk.gdk.Color(0,65535,0))
+				self.mutes[channel].modify_bg(gtk.STATE_ACTIVE, gtk.gdk.Color(0,55535,0))
+				self.mutes[channel].modify_bg(gtk.STATE_PRELIGHT, gtk.gdk.Color(0,65535,0))
 
 	def __init__(self, nchannels, host, port, protocol, instance, local_port):
 		self.lock = threading.Lock()
@@ -284,72 +267,71 @@ class VolumeGUI:
 		self.mutes = [None]*self.nchannels
 		self.muted = [None]*self.nchannels
 
-#		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
-		self.window = Gtk.Window()
+		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
 		self.window.connect("delete_event", self.delete_event)
 		self.window.connect("destroy", self.destroy)
 		self.window.set_border_width(10)
 
-		self.table = Gtk.Table(4, channels)
+		self.table = gtk.Table(4, channels)
 
 		for i, val in enumerate(self.vscales):
-			self.vscales[i] = Gtk.Scale.new_with_range(Gtk.Orientation.VERTICAL, 0.0, 1.0, 0.01)
+			self.vscales[i] = gtk.VScale()
 			self.vscales[i].set_digits(2)
 			self.vscales[i].set_draw_value(False)
-			#self.vscales[i].set_range(0, 1)
+			self.vscales[i].set_range(0, 1)
 			self.vscales[i].set_value(db_to_fader(0.0))
 			self.vscales[i].set_inverted(True)
+#			self.vscales[i].add_mark(100, gtk.POS_RIGHT, "100")
+#			self.vscales[i].add_mark(75, gtk.POS_RIGHT, "75")
+#			self.vscales[i].add_mark(50, gtk.POS_RIGHT, "50")
+#			self.vscales[i].add_mark(25, gtk.POS_RIGHT, "25")
+#			self.vscales[i].add_mark(0, gtk.POS_RIGHT, "0")
 			self.vscales[i].connect("change-value", self.scale_event)
 			self.table.attach(self.vscales[i], i, i+1, 2, 3)
 			self.vscales[i].show()
 
-			self.mutes[i] = Gtk.Button("Mute")
+			self.mutes[i] = gtk.Button("Mute")
 			self.set_mute(i, False)
 #			self.send_channel_mute(i, False)
 			self.mutes[i].connect("clicked", self.click_mute)
-#			self.table.attach(self.mutes[i], i, i+1, 0, 1, gtk.SHRINK, gtk.SHRINK)
-			self.table.attach(self.mutes[i], i, i+1, 0, 1, Gtk.AttachOptions.SHRINK, Gtk.AttachOptions.SHRINK)
+			self.table.attach(self.mutes[i], i, i+1, 0, 1, gtk.SHRINK, gtk.SHRINK)
 			self.mutes[i].show()
 
-			self.dbs[i] = Gtk.Entry()
+			self.dbs[i] = gtk.Entry(0)
 			self.dbs[i].set_text("0.0")
 			self.dbs[i].set_width_chars(4)
 			self.dbs[i].connect("activate", self.activate_event)
 			self.dbs[i].show()
-#			self.table.attach(self.dbs[i], i, i+1, 1, 2, gtk.SHRINK, gtk.SHRINK)
-			self.table.attach(self.dbs[i], i, i+1, 1, 2, Gtk.AttachOptions.SHRINK, Gtk.AttachOptions.SHRINK)
+			self.table.attach(self.dbs[i], i, i+1, 1, 2, gtk.SHRINK, gtk.SHRINK)
 			if (i != self.nchannels-1):
 #				self.send_channel_osc(i, db_to_coeff(0.0))
-				label = Gtk.Label("Channel "+str(i+1))
-#				self.table.attach(label, i, i+1, 3, 4, gtk.SHRINK, gtk.FILL)
-				self.table.attach(label, i, i+1, 3, 4, Gtk.AttachOptions.SHRINK, Gtk.AttachOptions.FILL)
+				label = gtk.Label("Channel "+str(i+1))
+				self.table.attach(label, i, i+1, 3, 4, gtk.SHRINK, gtk.FILL)
 				label.show()
 #self.table.attach(self.vscales[i], i, i+1, 0, 1)
 
 #		self.send_master_osc(db_to_coeff(0.0))
 #		self.send_master_mute(False)
 
-		master = Gtk.Label("Master")
-#		self.table.attach(master, self.nchannels-1, self.nchannels, 3, 4, gtk.FILL, gtk.FILL)
-		self.table.attach(master, self.nchannels-1, self.nchannels, 3, 4, Gtk.AttachOptions.FILL, Gtk.AttachOptions.FILL)
+		master = gtk.Label("Master")
+		self.table.attach(master, self.nchannels-1, self.nchannels, 3, 4, gtk.FILL, gtk.FILL)
 		master.show()
 		
+
 		self.window.add(self.table)
 		self.window.set_size_request(self.nchannels*90, 300)
-#		self.window.set_position(gtk.WIN_POS_CENTER)
-#		self.window.set_position(Gtk.WindowPosition.CENTER)
+		self.window.set_position(gtk.WIN_POS_CENTER)
 		self.table.show()
 		self.window.show()
 
 	def main(self):
-		Gtk.main()
+		gtk.main()
 
 	def get_nchannels(self):
 		return self.nchannels
 
 
 if __name__ == "__main__":
-	signal.signal(signal.SIGINT, signal.SIG_DFL)
 	channels = 2
 	port = 7600
 	host = "localhost"
